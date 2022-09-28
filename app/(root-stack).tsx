660640ab6feb7a4cddec08a9827092ad6da825d6
Albert Gao <albertgaohy@gmail.com>
2022-09-28 02:49:03 +0100

    added stack, caused problem

--- app/(root-stack).tsx
+++ app/(root-stack).tsx
@@ -3,23 +3,21 @@ import { Platform, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ThemeProvider } from "../components/ThemeProvider";
 import { Suspense } from "react";
-
-const Header = () => {
-  return <Text>Header</Text>;
-};
+import { StacksProvider } from "@mobily/stacks";
 
 export default function RootStack() {
   return (
     <SafeAreaProvider>
       <ThemeProvider>
-        <Header />
-        <Suspense fallback="loading...">
-          {Platform.OS === "web" ? (
-            <Children />
-          ) : (
-            <Stack screenOptions={{ headerShown: false }} />
-          )}
-        </Suspense>
+        <StacksProvider>
+          <Suspense fallback="loading...">
+            {Platform.OS === "web" ? (
+              <Children />
+            ) : (
+              <Stack screenOptions={{ headerShown: false }} />
+            )}
+          </Suspense>
+        </StacksProvider>
       </ThemeProvider>
     </SafeAreaProvider>
   );
