9ee7035aa858eb8fb532455c3297f9939e616d29
Albert Gao <albertgaohy@gmail.com>
2022-10-01 00:17:26 +0100

    updated to the latest version

--- app/(non-auth).tsx
+++ app/(non-auth).tsx
@@ -1,18 +1,21 @@
 import { Children, Stack } from "expo-router";
 import { Platform } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { AuthProvoider } from "../components/AuthProvider";
 import { ThemeProvider } from "../components/ThemeProvider";
 
 export default function NonAuthLayout() {
   return (
-    <SafeAreaProvider>
-      <ThemeProvider>
-        {Platform.OS === "web" ? (
-          <Children />
-        ) : (
-          <Stack screenOptions={{ headerShown: false }} />
-        )}
-      </ThemeProvider>
-    </SafeAreaProvider>
+    <AuthProvoider>
+      <SafeAreaProvider>
+        <ThemeProvider>
+          {Platform.OS === "web" ? (
+            <Children />
+          ) : (
+            <Stack screenOptions={{ headerShown: false }} />
+          )}
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </AuthProvoider>
   );
 }
